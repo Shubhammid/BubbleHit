ce04a33f5e94c026b467909051c807f5ff1d2e5c
Shubhammid <131759573+Shubhammid@users.noreply.github.com>
2023-08-24 12:12:19 +0530

    All html css js files

---
+++ script.js
@@ -0,0 +1,63 @@
+var time=120;
+var score=0;
+var hs=0;
+
+function MakeBubble()
+{
+    var clutter=" ";
+   for(var i=1; i<=175;i++)
+  {   
+    var rn= Math.floor(Math.random()*10)
+    var clutter = clutter + ` <div id="bubble">${rn}</div> ` ;
+  }
+
+    document.querySelector("#pbottom").innerHTML= clutter;
+}
+
+function newHit()
+{
+    hs=Math.floor(Math.random()*10);
+    document.querySelector("#hitVal").textContent=hs;
+}
+
+
+function runtimer()
+{
+    var timee=setInterval(function()
+    {
+        if(time>0)
+        {
+            time--;
+            document.querySelector("#timerval").textContent=time;
+        }
+        else
+        {
+           clearInterval(timee);
+           document.querySelector("#pbottom").innerHTML=`<h1>Game Over </h1>`;
+        }
+
+    } ,1000 ) ;
+}
+
+
+function scoreIncrease()
+{
+   score += 10;
+   document.querySelector("#scoreVal").textContent=score;
+}
+
+document.querySelector("#pbottom").addEventListener("click",function(details)
+{
+   var clicknum = Number(details.target.textContent);     //if output is in black it is string ......and if output is in blue it is number
+   if(clicknum === hs)
+   {
+     scoreIncrease();
+     MakeBubble();
+     newHit();
+   }
+});
+
+
+newHit();
+runtimer();
+MakeBubble();
